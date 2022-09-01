fdcb0a6db4d07e01329a5de249eae1b12280ceee
Daryl Santillan <johns@getchoice.com>
2022-09-01 13:51:49 -0500

    working on the database

--- server/API/Program.cs
+++ server/API/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace API
 {
@@ -13,6 +15,14 @@ namespace API
     {
         public static void Main(string[] args)
         {
+            MongoClient client = new MongoClient("mongodb+srv://jdsantil:Jbdeha5z*@cluster0.yhkrx.mongodb.net/?retryWrites=true&w=majority");
+
+            List<string> databases = client.ListDatabaseNames().ToList();
+
+            foreach (string database in databases)
+            {
+                Console.WriteLine(database);
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
---
+++ server/Application/Services/MongoDBServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Application.Services
+{
+    public class MongoDBServices
+    {
+        //private readonly IMongoCollection<Accounts>
+    }
+}
